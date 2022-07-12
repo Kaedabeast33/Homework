9c8555c560e3084257fc123378400e68f422fe3e
Kaedabeast33 <109103212+Kaedabeast33@users.noreply.github.com>
2022-07-12 12:09:23 -0600

    Add files via upload

---
+++ challenge1.js
@@ -0,0 +1,19 @@
+console.log("Hello World!")
+
+let x = 7
+let y = 2
+
+const largest = Math.max(x,y)
+console.log("the largest number is " + largest);
+
+console.log
+
+//const num1 = parseFloat(prompt("1"));
+//const num2 = parseFloat(prompt("2"));
+//const num3 = parseFloat(prompt("3"));
+//let largest;
+
+//if(num1 >= num2 && num1 >= num3) 
+//{largest = num1;
+//}
+//else if (num2 >= num1 && num2 >= num3) { largest} 
